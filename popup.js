8a581c6013f24ff889566bdeebb149c94435a067
amiribnjaman <amiribnjaman@gmail.com>
2022-05-24 01:25:45 +0600

    A clock extension implemented

--- popup.js
+++ popup.js
@@ -1,6 +1,21 @@
-let changeColor = document.getElementById("changeColor");
+const date = new Date()
+const sec = date.getSeconds()
+const min = date.getMinutes()
+const hours = date.getHours()
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// const second = document.getElementById("second")
+// const minute = document.getElementById("minute")
+// second.innerHTML = sec + ' Meconds'
+// minute.innerHTML = hours + ' Hour'
+const amPm = document.getElementById("time")
+amPm.innerHTML = setAmPm(min, hours, sec)
 
+
+function setAmPm(minutes, hours, sec) {
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  const strTime = hours + ':' + minutes + ':' + sec + ' ' + ampm;
+  return strTime;
+}
