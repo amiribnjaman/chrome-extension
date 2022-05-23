3f1a7c8c74ebdb7790418cf3c01c4551a6fa2b85
amiribnjaman <amiribnjaman@gmail.com>
2022-05-24 00:56:00 +0600

    A simple chrome extension

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome?.action?.setBadgeText({ text: 'ON' });
+chrome.action.setBadgeBackgroundColor({ color: '#4688F1' });
+chrome.action.setCurosr({ cursor: 'pointer' });
---
+++ popup.js
@@ -0,0 +1,6 @@
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
